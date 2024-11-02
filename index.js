0045fd58f63a18a97a8fa89cb199bacf46ca85a1
VISHNUPREMa <vishnuprem5152@gmail.com>
2024-11-02 17:11:13 +0530

    git ignore added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("sample");
+// Import the Express module
+const express = require('express');
+
+// Create an instance of an Express application
+const app = express();
+
+// Define the port the server will run on
+const PORT = 3000;
+
+// Define the homepage route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Homepage!');
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
