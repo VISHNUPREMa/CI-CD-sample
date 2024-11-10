1562d447edd248e8762cd356f317ca54274d10e5
VISHNUPREMa <vishnu@growhut.in>
2024-11-10 13:54:30 +0530

    second commit

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Homepage!');
 });
 
+app.get('/login', (req, res) => {
+  res.send('Welcome to the Login page!');
+});
+
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
