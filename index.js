c3abbec4f6b7c02d0c0d96c2a65caac1cfb49ed6
VISHNUPREMa <vishnu@growhut.in>
2024-11-10 22:31:41 +0530

    home page added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import the Express module
 const express = require('express');
+const path = require('path');
 
 // Create an instance of an Express application
 const app = express();
@@ -7,24 +8,18 @@ const app = express();
 // Define the port the server will run on
 const PORT = 3000;
 
+// Set the views folder and view engine (EJS)
+app.set('views', path.join(__dirname, 'views')); // Set views folder
+app.set('view engine', 'ejs'); // Use EJS as the templating engine
+
 // Define the homepage route
 app.get('/', (req, res) => {
-  res.send('Welcome to the Homepage!');
+  res.render('home'); // Renders home.ejs from the views folder
 });
 
-app.get('/login', (req, res) => {
-  res.send('Welcome to the Login page!');
-});
 
-app.get('/shop', (req, res) => {
-  res.send('Welcome to the shop page!');
-});
 
-app.get('/customer', (req, res) => {
-  res.send('Welcome to the customer page!');
-});
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
